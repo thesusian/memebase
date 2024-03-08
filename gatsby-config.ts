3a9371c150c4cc45af06c3b4b599bae6447e2322
Mohammad Alsoussi <alsoussi@protonmail.com>
2024-03-08 23:03:39 +0300

    Configurating things

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,27 +1,40 @@
 import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
-  siteMetadata: {
-    title: `Memesbase`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
-  graphqlTypegen: true,
-  plugins: ["gatsby-plugin-sass", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
-    },
-    __key: "images"
-  }]
+	siteMetadata: {
+		title: "Coomer Memes",
+    description: "Curated coomer memes for your enjoyment",
+    image: "coomer.jpg",
+		siteUrl: "https://coomer.org",
+    
+	},
+	// More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+	// If you use VSCode you can also use the GraphQL plugin
+	// Learn more at: https://gatsby.dev/graphql-typegen
+	graphqlTypegen: true,
+	plugins: [
+		"gatsby-plugin-sass",
+		"gatsby-plugin-image",
+		"gatsby-plugin-sitemap",
+		{
+			resolve: "gatsby-plugin-manifest",
+			options: {
+				name: "Coomer Memes",
+				start_url: '/',
+				icon: "src/images/coomer.jpg",
+			},
+		},
+		"gatsby-plugin-sharp",
+		"gatsby-transformer-sharp",
+		{
+			resolve: "gatsby-source-filesystem",
+			options: {
+				name: "images",
+				path: "./src/images/",
+			},
+			__key: "images",
+		},
+	],
 };
 
 export default config;
---
+++ src/types/typings.d.ts
@@ -0,0 +1,1 @@
+declare module "*"; // so typesctipt can shutup
